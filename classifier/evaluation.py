5c155d3180688900dfdf8e91976dd8a52c5445f6
Vivek Vinushanth <cvivekvinushanth@gmail.com>
2020-06-17 08:05:45 +0530

    -more preprocessing

--- classifier/evaluation.py
+++ classifier/evaluation.py
@@ -1,69 +1,116 @@
 """Class to compare performance with different classifiers"""
 import sys
 
+from matplotlib import pyplot
+from numpy import sort
+from sklearn.feature_selection import SelectFromModel
+from xgboost import plot_importance
+
 sys.path.append('../../')
 # sys.path.append('/content/Modified-Geometric-Smote/')
 import numpy as np
 import pandas as pd
 import xgboost as xgb
 # import applications.main as gsom
-from classifier.preprocessing import pre_process as pp, getTest
+from classifier.preprocessing import pre_process as pp, getTest, pre_process_chain as ppc, pre_process2 as pp2
+from classifier.preprocessing import get_not_all_encode as pp_not_encode, get_not_all_encode_test as pp_test_not_encode
+from sklearn.multioutput import ClassifierChain
 
 #  Directory
 path = '../../data/'
 
-# def getTest(filename):
-#     df = pd.read_csv(filename)
-#
-#     X = np.asarray(df.iloc[1:-1].values)
-#
-#
-#     scaler = MinMaxScaler()
-#     X = scaler.fit_transform(X)
-#     return X, index
 
-def evaluate(classifier,y_predict,index):
+def evaluate(classifier, y_predict, index):
     # Write output to csv file
-    print(classifier)
+    index.resize(26708, 1)
+    data = np.column_stack([index, y_predict])
+    label = ["respondent_id", "h1n1_vaccine"]
+    frame = pd.DataFrame(data, columns=label)
+    export_csv = frame.to_csv(r'../output/h1n1_feature.csv', header=True)
 
-    print(index.shape)
-    index.resize(26708,1)
-    print(y_predict.shape)
-    data = np.column_stack([index,y_predict])
-    label= ["respondent_id","h1n1_vaccine"]
-    frame  = pd.DataFrame(data,columns=label)
-    export_csv = frame.to_csv(r'output/h1n1_vaccine.csv',header=True)
 
-def XGBoost(X_train,y_train,X_test):
-
+def XGBoost(X_train, y_train, X_test):
     # Fitting X-Gradient boosting
     gbc = xgb.XGBClassifier(objective="binary:logistic", random_state=42)
     gbc.fit(X_train, y_train)
 
     # Predicting the Test set results
     y_predict = gbc.predict_proba(X_test)
-    y_predict = y_predict[:,1]
+    y_predict = y_predict[:, 1]
+    return evaluate("XGBoost", y_predict, index)
+
+# define custom class to fix bug in xgboost 1.0.2
+class MyXGBClassifier(xgb.XGBClassifier):
+	@property
+	def coef_(self):
+		return None
+
+def XGBoost_feature_selection(X_train, y_train, X_test):
+
+    # Fitting X-Gradient boosting
+    gbc = MyXGBClassifier(objective="binary:logistic", random_state=42,importance_type="gain")
+    gbc.fit(X_train, y_train)
+    # plot feature importance
+    plot_importance(gbc)
+    pyplot.show()
+
+    # # Fit model using each importance as a threshold
+    thresholds = sort(gbc.feature_importances_)
+    
+    print (thresholds.shape[0])
+    print(thresholds)
+    thresh=thresholds[-10]
+    print(thresh)
+    selection = SelectFromModel(gbc, threshold=thresh, prefit=True)
+    select_X_train = selection.transform(X_train)
+    print(select_X_train.shape[1])
+
+    # train model
+    selection_model = xgb.XGBClassifier(objective="binary:logistic", random_state=42)
+    selection_model.fit(select_X_train, y_train)
+    # eval model
+    select_X_test = selection.transform(X_test)
+
+    # Predicting the Test set results
+    y_predict = selection_model.predict_proba(select_X_test)
+    y_predict = y_predict[:, 1]
+
     return evaluate("XGBoost", y_predict, index)
 
 
 
+def XGBoostChain(X_train, y_train, X_test):
+    print("fitting the data")
+    # Fitting X-Gradient boosting
+    gbc = xgb.XGBClassifier(objective="binary:logistic", random_state=42)
+
+    chains = [ClassifierChain(gbc, order='random', random_state=i) for i in range(10)]
+
+    for chain in chains:
+        chain.fit(X_train, y_train)
+
+    Y_pred_chains = np.array([chain.predict_proba(X_test) for chain in chains])
+    Y_pred_ensemble = Y_pred_chains.mean(axis=0)
+    print(Y_pred_ensemble)
+    # y_predict = y_predict[:,1]
+    # return evaluate("XGBoost", y_predict, index)
+
 
 # data transformation if necessary.
-X_t, y_t = pp('training_set_h1n1.csv')
+# X_t, y_t = pp('training_set_h1n1.csv')
+
+# Pre-process for chain
+# X_t, y_t = pp2('train_h1n1.csv')
+X_t, y_t = pp_not_encode('train_h1n1.csv')
 
 # For Non-resampled
-X_train,y_train = X_t,y_t
+X_train, y_train = X_t, y_t
 
-X_test,index = getTest('test_set_features.csv')
+X_test, index = pp_test_not_encode('test.csv')
+# X_test,index = getTest('test_set_features_chain.csv')
 
-# For SMOTE
-# sm = SMOTE()
-# X_train, y_train = sm.fit_resample(X_t, y_t)
+# XGBoost(X_train, y_train, X_test)
+XGBoost_feature_selection(X_train, y_train, X_test)
 
-# GSMOTE = EGSmote()
-# # GSMOTE = OldGeometricSMOTE()
-# X_train, y_train = GSMOTE.fit_resample(X_t, y_t)
-
-XGBoost(X_train,y_train,X_test)
-
-
+# Chain of classifiers
+# XGBoostChain(X_train,y_train,X_test)
--- classifier/preprocessing.py
+++ classifier/preprocessing.py
@@ -4,11 +4,14 @@ import sys
 import pandas as pd
 import numpy as np
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler, StandardScaler
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+
 
 def pre_process(filename):
     path = "data/"
-    df = pd.read_csv(path+filename)
+    df = pd.read_csv(path + filename)
 
     # Fill the NaN with mean
     # df.fillna(df.mean(), inplace=True)
@@ -20,7 +23,6 @@ def pre_process(filename):
 
     X = df.iloc[:, 1:-1].values
     y = df.iloc[:, -1].values
-
 
     columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(drop='first'), [21])], remainder='passthrough')
     X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
@@ -60,10 +62,11 @@ def pre_process(filename):
 
     scaler = MinMaxScaler()
     X = scaler.fit_transform(X)
-    return X,y
+    return X, y
+
 
 def getTest(filename):
-    filename = "data/"+filename
+    filename = "data/" + filename
     df = pd.read_csv(filename)
 
     # Fill the NaN with mode
@@ -102,12 +105,223 @@ def getTest(filename):
     columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(drop='first'), [47])], remainder='passthrough')
     X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
 
-    columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(drop='first'), [51])], remainder='passthrough')
+    # columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(drop='first'), [51])], remainder='passthrough')
+    # X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
+    #
+    # columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(drop='first'), [71])], remainder='passthrough')
+    # X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
+
+    scaler = MinMaxScaler()
+    X = scaler.fit_transform(X)
+    return X, index
+
+
+def pre_process_chain(filename):
+    path = "data/"
+    df = pd.read_csv(path + filename)
+
+    # Fill the NaN with mean
+    # df.fillna(df.mean(), inplace=True)
+
+    # Fill the NaN with mode
+    df.fillna(df.mode().iloc[0], inplace=True)
+
+    # df = df.drop_duplicates(df.iloc[:, :-1].columns, keep='last')
+
+    X = df.iloc[:, 1:-1].values
+    y = df.iloc[:, -1:-3].values
+
+    columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(drop='first'), [21])], remainder='passthrough')
     X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
 
-    columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(drop='first'), [71])], remainder='passthrough')
+    columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(drop='first'), [25])], remainder='passthrough')
+    X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
+
+    columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(drop='first'), [28])], remainder='passthrough')
+    X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
+
+    columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(drop='first'), [31])], remainder='passthrough')
+    X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
+
+    columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(drop='first'), [32])], remainder='passthrough')
+    X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
+
+    columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(drop='first'), [34])], remainder='passthrough')
+    X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
+
+    columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(drop='first'), [35])], remainder='passthrough')
+    X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
+
+    columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(drop='first'), [36])], remainder='passthrough')
+    X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
+
+    columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(drop='first'), [38])], remainder='passthrough')
+    X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
+
+    columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(drop='first'), [47])], remainder='passthrough')
     X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
 
     scaler = MinMaxScaler()
     X = scaler.fit_transform(X)
-    return X,index
+    print("pre-processing done")
+
+    return X, y
+
+
+def pre_process2(filename):
+    path = "data/"
+    df = pd.read_csv(path + filename)
+
+    # Fill the NaN with mean
+    # df.fillna(df.mean(), inplace=True)
+
+    # Fill the NaN with mode
+    df.fillna(df.mode().iloc[0], inplace=True)
+
+    # df = df.drop_duplicates(df.iloc[:, :-1].columns, keep='last')
+
+    X = df.iloc[:, 1:-1].values
+    y = df.iloc[:, -1].values
+
+    columnTransformer = ColumnTransformer(
+        [('encoder', OneHotEncoder(drop='first'), [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 29, 30])],
+        remainder='passthrough')
+    X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
+
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+    return X, y
+
+
+def getTest(filename):
+    filename = "data/" + filename
+    df = pd.read_csv(filename)
+
+    # Fill the NaN with mode
+    df.fillna(df.mode().iloc[0], inplace=True)
+
+    X = df.iloc[:, 1:].values
+    index = np.asarray(df.iloc[:, 0].values)
+
+    columnTransformer = ColumnTransformer(
+        [('encoder', OneHotEncoder(drop='first'), [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 29, 30])],
+        remainder='passthrough')
+    X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
+
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+    return X, index
+
+
+def get_not_all_encode(filename):
+    filename = "../data/" + filename
+    print(filename)
+    df = pd.read_csv(filename)
+
+    # Fill the NaN with mode
+    df["age_group"].fillna("unknown1", inplace=True)
+    df["education"].fillna("unknown2", inplace=True)
+    df["race"].fillna("unknown3", inplace=True)
+    df["sex"].fillna("unknown4", inplace=True)
+    df["income_poverty"].fillna("unknown5", inplace=True)
+    df["marital_status"].fillna("unknown6", inplace=True)
+    df["rent_or_own"].fillna("unknown7", inplace=True)
+    df["employment_status"].fillna("unknown8", inplace=True)
+    df["hhs_geo_region"].fillna("unknown9", inplace=True)
+    df["census_msa"].fillna("unknown10", inplace=True)
+    # df["employment_industry"].fillna("unknown11", inplace=True)
+    # df["employment_occupation"].fillna("unknown12", inplace=True)
+
+    df.fillna(df.mode().iloc[0], inplace=True)
+    # print(df.head(15))
+    X = df.iloc[:, 1:-1].values
+    y = df.iloc[:, -1].values
+
+    columnTransformer = ColumnTransformer(
+        [('encoder', OneHotEncoder(drop='first'), [21, 22, 23, 24, 25, 26, 27, 28, 29, 30])], remainder='passthrough')
+    X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
+
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+    return X, y
+
+
+def get_not_all_encode_test(filename):
+    filename = "../data/" + filename
+    df = pd.read_csv(filename)
+
+    # Fill the NaN with mode
+    # df.fillna(df.mode().iloc[0], inplace=True)
+    # df.fillna({'a': 0, 'b': 0}, inplace=True)
+
+    df["age_group"].fillna("unknown1", inplace=True)
+    df["education"].fillna("unknown2", inplace=True)
+    df["race"].fillna("unknown3", inplace=True)
+    df["sex"].fillna("unknown4", inplace=True)
+    df["income_poverty"].fillna("unknown5", inplace=True)
+    df["marital_status"].fillna("unknown6", inplace=True)
+    df["rent_or_own"].fillna("unknown7", inplace=True)
+    df["employment_status"].fillna("unknown8", inplace=True)
+    df["hhs_geo_region"].fillna("unknown9", inplace=True)
+    df["census_msa"].fillna("unknown10", inplace=True)
+    # df["employment_industry"].fillna("unknown11", inplace=True)
+    # df["employment_occupation"].fillna("unknown12", inplace=True)
+
+    df.fillna(df.mode().iloc[0], inplace=True)
+
+    # print(df.head(15))
+
+    X = df.iloc[:, 1:].values
+    index = np.asarray(df.iloc[:, 0].values)
+
+    columnTransformer = ColumnTransformer(
+        [('encoder', OneHotEncoder(drop='first'), [21, 22, 23, 24, 25, 26, 27, 28, 29, 30])], remainder='passthrough')
+    X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
+
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+    return X, index
+
+
+def feature_selection(filename):
+    filename = "../data/" + filename
+    df = pd.read_csv(filename)
+    features_df = df.iloc[:, 1:-1].values
+    labels = df.iloc[:, -1].values
+
+    df["age_group"].fillna("unknown1", inplace=True)
+    df["education"].fillna("unknown2", inplace=True)
+    df["race"].fillna("unknown3", inplace=True)
+    df["sex"].fillna("unknown4", inplace=True)
+    df["income_poverty"].fillna("unknown5", inplace=True)
+    df["marital_status"].fillna("unknown6", inplace=True)
+    df["rent_or_own"].fillna("unknown7", inplace=True)
+    df["employment_status"].fillna("unknown8", inplace=True)
+    df["hhs_geo_region"].fillna("unknown9", inplace=True)
+    df["census_msa"].fillna("unknown10", inplace=True)
+    # df["employment_industry"].fillna("unknown11", inplace=True)
+    # df["employment_occupation"].fillna("unknown12", inplace=True)
+
+    df.fillna(df.mode().iloc[0], inplace=True)
+
+
+    features_df = df.iloc[:, 1:].values
+
+    columnTransformer = ColumnTransformer(
+        [('encoder', OneHotEncoder(drop='first'), [21, 22, 23, 24, 25, 26, 27, 28, 29, 30])], remainder='passthrough')
+
+    features_df = np.array(columnTransformer.fit_transform(features_df), dtype=np.str)
+
+    scaler = StandardScaler()
+    features_df = scaler.fit_transform(features_df)
+
+    bestfeatures = SelectKBest(score_func=chi2, k=10)
+    fit = bestfeatures.fit(features_df, labels)
+    dfscores = pd.DataFrame(fit.scores_)
+    dfcolumns = pd.DataFrame(features_df.columns)
+    # concat two dataframes for better visualization
+    featureScores = pd.concat([dfcolumns, dfscores], axis=1)
+    featureScores.columns = ['Specs', 'Score']  # naming the dataframe columns
+    print(featureScores.nlargest(35, 'Score'))  # print 10 best features
+
+# feature_selection("train_h1n1.csv")
